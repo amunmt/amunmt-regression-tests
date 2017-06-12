f9d6f78a9c15d70a69307461e3ed880acfc4f688
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2017-06-12 13:52:53 +0000

    Update paths and name: amunmt->marian

--- tests/test_python_bindigs/test.py
+++ tests/test_python_bindigs/test.py
@@ -4,7 +4,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../../../amunmt/build/src')
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../../../marian/build/src')
 import libamunmt as nmt
 
 
