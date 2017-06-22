912e052ec9756347da7423b14cf74d1e35ede77c
Hieu Hoang <s0565741@odin.inf.ed.ac.uk>
2017-06-22 05:12:22 +0100

    don't ignore test_python_bindigs

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -ex
 
 for test in `ls tests`; do
-  if [ "$test" != "test_python_bindigs" ]; then
-	cd tests/$test
-	make 2> err
-	cd -
-  fi
+  cd tests/$test
+  make 2> err
+  cd -
 done
