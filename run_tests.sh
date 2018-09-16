f7a3148f86d80610d800e5485a34d2a67cb3bd3c
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-09-16 16:50:26 +0100

    Check for ../amun/build/amun

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+if [ ! -s ../amun/build/amun ]; then
+  echo "Could not find amun executable: ../amun/build/amun"
+  exit 1
+fi
+
 for test in `ls tests`; do
   cd tests/$test
   time -p make -i 2> err
