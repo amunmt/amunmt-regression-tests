e6a362d1cbdbe2aa8afce6ed5eb2d123b94dc7dc
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-09-16 18:00:57 +0100

    Allow running specific tests: run_tests.sh tests/<names>

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -ex
 
+if [ $# -eq 0 ]; then
+    TESTS='tests/*'
+else
+    TESTS=$@
+fi
+
 if [ ! -s ../amun/build/amun ]; then
   echo "Could not find amun executable: ../amun/build/amun"
   exit 1
 fi
 
-for test in `ls tests`; do
-  cd tests/$test
+for test in `ls -d $TESTS`; do
+  cd $test
   time -p make -i 2> err
   cd -
 done
